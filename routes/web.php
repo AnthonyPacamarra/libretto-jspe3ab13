053b68d19636d4997e61b5df05fdbfb57c6bf352
AnthonyPacamarra <anthonygwapopacamarra@gmail.com>
2024-06-18 20:29:23 +0800

    separated first laravel project

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RelationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', ProductController::class);
 Route::get('/author/books/{id}', [RelationController::class, 'getBooksByAuthor']);
